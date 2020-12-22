32e88e47a83029cb9c564455f328bba1d94516ea
sitcomdeveloper <sitcomdeveloper43@gmail.com>
2020-12-22 16:40:21 +0530

    Rohan latest commit 22-12-2020

--- src/app/index/home/home.component.ts
+++ src/app/index/home/home.component.ts
@@ -7,20 +7,9 @@ import { ApiService } from 'src/api.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  fetchImagesofHeaders: any;
-  images: any;
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit() {
-    this.HeaderSliders();
   }
-HeaderSliders() {
-  const headersImages = {}
-  this.apiService.getheaderSlider(headersImages).subscribe(imagesHeadersRes => {
-    this.fetchImagesofHeaders = imagesHeadersRes;
-    this.images = this.fetchImagesofHeaders.imageData;
-    console.log('fetchImagesofHeaders', this.images);
-  })
-}
 }
